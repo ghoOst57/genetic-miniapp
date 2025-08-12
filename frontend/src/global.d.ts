e86d77788a357ccace67f58b46a83c100ae08f3f
Dmitrii <igromanchig@gmail.com>
2025-08-12 12:26:28 +0300

    fix(front): minimal vite config + globals + relax build

---
+++ frontend/src/global.d.ts
@@ -0,0 +1,16 @@
+
+export {};
+
+declare global {
+  interface Window {
+    Telegram?: any;
+    __API_BASE__?: string;
+  }
+}
+
+interface ImportMetaEnv {
+  readonly VITE_API_BASE?: string;
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
